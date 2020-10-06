324c4a825a39665ec63f3d0c1a1aff6b6906c3a3
Lauren Parsons <laurenparsons516@gmail.com>
2020-10-06 08:47:55 -0500

    Added searching ability

--- src/com/lauren/biblebook/Main.java
+++ src/com/lauren/biblebook/Main.java
@@ -2,16 +2,37 @@ package com.lauren.biblebook;
 
 import java.io.File;
 import  java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+//        Scanner input = new Scanner(new File("/home/lauren/IdeaProjects/BibleBook/src/com/lauren/biblebook/bibleBookList"));
+//        while(input.hasNext()){
+//            BibleBook s = new BibleBook(input.nextLine());
+//            s.displayReasonably();
+//        }
+//        input.close();
+
+        ArrayList<BibleBook> bibleBooks = new ArrayList<>();
         Scanner input = new Scanner(new File("/home/lauren/IdeaProjects/BibleBook/src/com/lauren/biblebook/bibleBookList"));
-        while(input.hasNext()){
-            BibleBook s = new BibleBook(input.nextLine());
-            s.displayReasonably();
+        while(input.hasNext()) {
+            BibleBook book = new BibleBook(input.nextLine());
+            bibleBooks.add(book);
         }
         input.close();
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter keyword: ");
+        String keyWord = userInput.nextLine();
+
+        for(int i = 0; i < bibleBooks.size(); i++){
+            BibleBook currentBook = bibleBooks.get(i);
+            if(currentBook.getContentSum().contains(keyWord)) {
+                System.out.println(currentBook.getBookName());
+                break;
+            }
+        }
 
     }
 }
